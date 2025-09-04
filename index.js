8817d9a1e4ead761789396037ec9f460c8905cc9
Shehryar Khan <shehryarkhattak001>
2025-09-04 23:15:47 +0500

    little changes

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 //* AJAX Asynchronous JavaScript And XML
 //! What is Asynchronous JavaScript
 //! Why do we use Asynchronous JavaScript
+
+console.log("Start");
+
+function otherFunction() {
+  console.log("We are in another function");
+  console.log("Do some stuff");
+}
+
+otherFunction();
+
+console.log("end");
